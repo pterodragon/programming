d151d26a36d46a3169eb13db6a68787c7f2d3716
Wing <poonlokwing@gmail.com>
2015-12-28 21:46:19 +0800

    changed lintcode Gas Station solution (fake AC)

--- C++/lintcode/Gas_Station/solution.cpp
+++ C++/lintcode/Gas_Station/solution.cpp
@@ -11,39 +11,27 @@ public:
 	 */
 	int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
 		auto const L = gas.size();
-		auto sum = accumulate(gas.begin(), gas.end(), 0) - accumulate(cost.begin(), cost.end(), 0);
-		if (sum < 0)
-			return -1;
-		int a = 0;
+		int sum = 0;
+		int total = 0;
+		int j = -1;
 		for (int i = 0; i < L; i++) {
-			if (gas[i] - cost[i] >= 0) {
-				a = i;
-				break;
+			auto diff = gas[i] - cost[i];
+			sum += diff;
+			total += diff;
+			if (sum < 0) {
+				j = i;
+				sum = 0;
 			}
 		}
-		int b = a;
-		auto diff = [&gas, &cost](int x) {return gas[x] - cost[x];};
-		int gas_remain = diff(a);
-		while ((b + 1) % L != a) {
-			if (diff((b + 1) % L) + gas_remain >= 0) {
-				b = (b + 1) % L;
-				gas_remain += diff(b);
-			} else if (diff((L + a - 1) % L) + gas_remain >= 0) {
-				a = (L + a - 1) % L;
-				gas_remain += diff(a);
-			} else {
-				return -1;
-			}
-		}
-		return a;
+		return total >= 0 ? j + 1 : -1;
 	}
 };
 
 int main() {
 	Solution s;
 
-	vector<vector<int>> gas { { 1, 1, 3, 1 } };
-	vector<vector<int>> cost { { 2, 2, 1, 1 } };
+	vector<vector<int>> gas { { 1, 1, 3, 1 }, { 0, 2, 0, 100 } };
+	vector<vector<int>> cost { { 2, 2, 1, 1 }, { 20, 1, 20, 0 } };
 	for (int i = 0; i < gas.size(); i++) {
 		auto ans = s.canCompleteCircuit(gas[i], cost[i]);
 		cout << ans << endl;
