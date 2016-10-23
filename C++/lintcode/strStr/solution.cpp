085f6b703c6f79ace98f9c1285424fd7eb756328
wing <poonlokwing@gmail.com>
2016-10-23 18:18:50 +0800

    corrected unchecked fail case of strStr, added test cases

--- C++/lintcode/strStr/solution.cpp
+++ C++/lintcode/strStr/solution.cpp
@@ -11,6 +11,7 @@ class Solution {
          * @param p string containing the sequence of characters to match.
          */
         int strStr(const char *t, const char *p) {
+            puts(__PRETTY_FUNCTION__);
             if (t == nullptr || p == nullptr) {
                 return -1;
             }
@@ -23,7 +24,7 @@ class Solution {
             int q = 0;
             for (int i = 0; i < n; ++i) {
                 while (q > 0 && p[q] != t[i]) {
-                    q = pi[q];
+                    q = pi[q - 1];
                 }
                 if (p[q] == t[i]) {
                     ++q;
@@ -36,23 +37,58 @@ class Solution {
         }
 
         vector<int> prefix(const char* p) {
+            puts(__PRETTY_FUNCTION__);
             int m = strlen(p);
             vector<int> pi(m);
             int k = 0;
             for (int q = 1; q < m; ++q) {
+                //printf("q = %d\n", q);
                 while (k > 0 && p[k] != p[q]) {
-                    k = pi[k];
+                    k = pi[k - 1];
                 }
                 if (p[k] == p[q]) {
                     ++k;
                 }
                 pi[q] = k;
+                //printf("pi[%d] = %d\n", q, k);
             }
+            print_prefix(pi, p);
             return pi;
+        }
+
+        void print_prefix(const vector<int>& pi, const char* p) {
+            puts(__PRETTY_FUNCTION__);
+            for (int i = 0; i < pi.size(); ++i) {
+                printf("%c, ", p[i]);
+            }
+            puts("");
+            for (int i = 0; i < pi.size(); ++i) {
+                printf("%d, ", pi[i]);
+            }
+            puts("");
         }
 };
 
 int main() {
-    int ans = Solution().strStr("abcdabcdefg", "bcd");
-    printf("ans = %d\n", ans);
+    array<tuple<string, string, int>, 7> a{{
+        make_tuple("aabaabcd", "abaabc", 1), 
+            make_tuple("cabaaabaabbb", "aaabaababa", -1),
+            make_tuple("aaabaaaabaaabaababa", "aaaabaaabaabab", 4),
+            make_tuple("zabcdefgzabcdefg", "abcdefgabcdefabcde", -1),
+            make_tuple("aaaaab", "aab", 3),
+            make_tuple("zzzzzzzzzz", "zzzzzzzzzz", 0),
+            make_tuple("aaabbbccc", "abbbc", 2),
+    }};
+
+    for (auto p : a) {
+        int ans = Solution().strStr(get<0>(p).c_str(), get<1>(p).c_str());
+        printf("t=%s, p=%s, first substring index = %d; assert = %d\n", get<0>(p).c_str(), get<1>(p).c_str(), ans, get<2>(p));
+        if (ans != get<2>(p)) {
+            printf("-------------------\n");
+            printf("--- TEST FAILED ---\n");
+            printf("-------------------\n");
+        }
+        puts("---");
+    }
+    return 0;
 }
