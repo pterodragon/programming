72ca2e5c70950d7318c936857425868894abc78a
wing <poonlokwing@gmail.com>
2017-04-25 23:20:57 +0800

    solved "Number of 1 Bits"

--- C++/leetcode/Number_of_1_Bits/solution.cpp
+++ C++/leetcode/Number_of_1_Bits/solution.cpp
@@ -1,3 +1,24 @@
+#include <cstdint>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        unsigned int count = 0;
+        do {
+            count += n & 1;
+        } while (n >>= 1);
+        return count;
+    }
+};
+
 int main() {
+    auto sol = Solution();
+    assert(sol.hammingWeight(1) == 1);
+    assert(sol.hammingWeight(0) == 0);
+    assert(sol.hammingWeight(0u - 1) == 32);
     return 0;
 }
