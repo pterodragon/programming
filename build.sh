7e64212e29c4765bad30c1dcca8db8b9bd5c1a43
wing <poonlokwing@gmail.com>
2017-04-27 22:55:12 +0800

    improved build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd build &&
-cmake ../C++ -DBoost_INCLUDE_DIR=~/Documents/boost_1_62_0/ -DCMAKE_CXX_FLAGS=-std=c++14 &&
-make &&
+cmake ../C++ -DBoost_INCLUDE_DIR=~/Documents/boost_1_62_0/ -DCMAKE_CXX_FLAGS=-std=c++14 -DCMAKE_BUILD_TYPE=Debug &&
+make -j9 &&
 cd -
