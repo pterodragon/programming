6312a2d54e316aa29af4122d76312285311e8f4a
wing <poonlokwing@gmail.com>
2017-04-29 17:01:00 +0800

    make AProject uses Boost

--- C++/other_projects/AProject/a.cpp
+++ C++/other_projects/AProject/a.cpp
@@ -1,17 +1,44 @@
-#include <fstream>
-#include <iostream>
+#include <bits/stdc++.h>
+#include <boost/format.hpp>
+
 
 using namespace std;
 
+template<typename X>
+X&& oh(typename remove_reference<X>::type& a) noexcept {
+    cout << __PRETTY_FUNCTION__ << '\n';
+    return static_cast<X&&>(a);
+} 
+
+template<typename X>
+X&& oh2(X& a) noexcept {
+    cout << __PRETTY_FUNCTION__ << '\n';
+    return static_cast<X&&>(a);
+} 
+
+void f(int& x) {
+    cout << __PRETTY_FUNCTION__ << '\n';
+    cout << x << '\n';
+}
+
+void f(int&& x) {
+    cout << __PRETTY_FUNCTION__ << '\n';
+    cout << x << '\n';
+}
+
+int get1() {
+    return 1;
+}
+
+template<typename T> 
+void g(T&& arg) { 
+    f(oh<T>(arg));
+    f(oh2(arg));
+} 
 int main() {
-    string line;
-    constexpr const char *filename = "reddit_post.txt";
-    ifstream myfile(filename);
-    cout << boolalpha << myfile.is_open() << '\n';
-    myfile.close();
-    cout << boolalpha << myfile.is_open() << '\n';
-    while (getline(myfile, line)) {
-        cout << line << '\n';
-    }
-    return 0;
+    cout << "int x = 1; g(x)" << '\n';
+    int x = 1;
+    g(x);
+    cout << "g(get1())" << '\n';
+    g(get1());
 }
