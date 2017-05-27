52a1a381118903d840cbfa109f8f221ea440ed60
wing <poonlokwing@gmail.com>
2017-05-27 18:36:44 +0800

    build script boost dependency points to third_party dir

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p build &&
 cd build &&
-cmake ../C++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBOOST_ROOT=~/Documents/boost_1_62_0/ -DCMAKE_CXX_FLAGS=-std=c++14 -DCMAKE_BUILD_TYPE=Debug &&
+cmake ../C++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBOOST_ROOT=$(pwd)/C++/third_party -DCMAKE_CXX_FLAGS=-std=c++14 -DCMAKE_BUILD_TYPE=Debug &&
 make -j9 $@ && # user can choose the targets
 cd -
