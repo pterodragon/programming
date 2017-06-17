1ef923b38fb27b7274f594a704bca509dbc8bafc
wing <poonlokwing@gmail.com>
2017-06-17 17:00:49 +0800

    leetcode "Predict the Winner" reduced space complexity

--- C++/leetcode/Predict_the_Winner/solution.hpp
+++ C++/leetcode/Predict_the_Winner/solution.hpp
@@ -5,21 +5,47 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     bool PredictTheWinner(vector<int>& nums) {
+//         if (nums.empty()) return true;
+//         const size_t L = nums.size();
+//         vector<vector<int>> dp(L, vector<int>(L));
+//         for (size_t i = 0; i < L; ++i) {
+//             dp[i][i] = nums[i];
+//         }
+//         for (size_t r = 1; r < L; ++r) { // r = subarray length - 1
+//             for (size_t i = 0; i + r < L; ++i) {
+//                 dp[i][i+r] = max(nums[i] - dp[i + 1][i+r], nums[i+r] - dp[i][i+r - 1]);
+//             }
+//         }
+//         return dp[0][L - 1] >= 0;
+//     }
+// };
+
+// reduced space complexity
+//    0    1    2    3
+// 0  n[0] ...
+// 1       n[1] ...
+// 2            n[2] ...
+// 3                 n[3]
+//
+// the next iteration (next diagonal depends on the previous diagonal only)
 class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         if (nums.empty()) return true;
         const size_t L = nums.size();
-        vector<vector<int>> dp(L, vector<int>(L));
+        vector<int> dp(L); // stores max score margin using nums[x:x + r + 1] where r increases in the loop
         for (size_t i = 0; i < L; ++i) {
-            dp[i][i] = nums[i];
+            dp[i] = nums[i];
         }
         for (size_t r = 1; r < L; ++r) { // r = subarray length - 1
             for (size_t i = 0; i + r < L; ++i) {
-                dp[i][i+r] = max(nums[i] - dp[i + 1][i+r], nums[i+r] - dp[i][i+r - 1]);
+                dp[i] = max(nums[i] - dp[i + 1], nums[i + r] - dp[i]);
             }
         }
-        return dp[0][L - 1] >= 0;
+        return dp[0] >= 0;
     }
 };
 
