7053edd528f030c09513a388b08f6789f7b78af4
wing <poonlokwing@gmail.com>
2018-09-16 21:44:10 +0900

    improved CMakeLists and build.sh; + ctest.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
-# to build any tests, specify the target as "${project_name}_test"
-mkdir -p build &&
-cd build &&
-cmake ../C++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBOOST_ROOT=$(pwd)/C++/third_party -DCMAKE_CXX_FLAGS=-std=c++14 -DCMAKE_BUILD_TYPE=Debug &&
-make -j9 $@ && # user can choose the targets
-cd -
+leetcode_only=""
+temp=""
+while getopts "ep:" o; do
+    case "${o}" in
+        e) # build leetcode only
+            leetcode_only="-Dbuild_leetcode_only=ON"
+            ;;
+        p)
+            temp=${OPTARG}
+            ;;
+
+    esac
+done
+shift $((OPTIND-1))
+
+target="$1"
+if [ -z $target ];
+then
+    target="all"
+fi
+
+cmake -H"C++" -Bbuild $leetcode_only -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBOOST_ROOT=$(pwd)/C++/third_party -DCMAKE_CXX_FLAGS=-std=c++17 -DCMAKE_BUILD_TYPE=Debug
+cmake --build build --config Debug --target $target -- -j 8
