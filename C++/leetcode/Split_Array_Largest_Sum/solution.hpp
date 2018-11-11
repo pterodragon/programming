cef0537d6f4d554c53fd99353ee0b0e491118a42
wing <poonlokwing@gmail.com>
2018-11-11 12:13:26 +0900

    more notes

--- C++/leetcode/Split_Array_Largest_Sum/solution.hpp
+++ C++/leetcode/Split_Array_Largest_Sum/solution.hpp
@@ -10,17 +10,17 @@ using namespace std;
 using P = array<long, 2>;
 
 class Solution {
-public:
-  int splitArray(vector<int> &nums, int m) {
+  // O(n^2, m); a better solution exists
+ public:
+  int splitArray(vector<int>& nums, int m) {
     const int N = nums.size();
-    if (N == 0)
-      return 0;
+    if (N == 0) return 0;
     long inf;
     memset(&inf, 0x3f, sizeof inf);
     vector<vector<P>> dp(N, vector<P>(m, {inf, inf}));
     auto ps = [&dp, &nums](int i, int j) {
       return dp[j][0][0] - dp[i][0][0] + nums[i];
-    }; // partial sum inclusive
+    };  // partial sum inclusive
     int sum = 0;
     for (int a = 0; a < N; ++a) {
       sum += nums[a];
